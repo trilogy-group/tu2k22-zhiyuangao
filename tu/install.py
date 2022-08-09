3e524ca7c2d73ccbec421d4b3b097a2a326cabfe
zhiyuangao <zhiyuan@trilogy.com>
2022-08-10 00:22:52 +0900

    update install script

--- tu/install.py
+++ tu/install.py
@@ -1,7 +1,10 @@
 
 sudo apt update
 sudo apt install mysql-server
-sudo service mysql start
+#sudo service mysql start
+sudo mysqld --skip-grant-tables
+#GIT_SSH_COMMAND='ssh -i /workspace/tu2k22-zhiyuangao/tu/id_trilogy -o IdentitiesOnly=yes'
 # CREATE USER 'sammy'@'localhost' IDENTIFIED BY 'password';
 # GRANT ALL PRIVILEGES ON *.* TO 'sammy'@'localhost' WITH GRANT OPTION;
 # FLUSH PRIVILEGES;
+# 
