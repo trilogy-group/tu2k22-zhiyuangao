c3a0e75f91653b71b3ef988bc04feb7823e5b91e
zhiyuangao <zhiyuan@trilogy.com>
2022-08-12 03:32:37 +0900

    support testing/tracing with open telemetry, jaegur and prom

--- tu/manage.py
+++ tu/manage.py
@@ -3,12 +3,14 @@
 import os
 import sys
 from django.core.management.commands.runserver import Command as runserver
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 
 
 def main():
     """Run administrative tasks."""
     runserver.default_port = "8080"
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu.settings')
+    DjangoInstrumentor().instrument()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ tu/run_prom.sh
@@ -0,0 +1,4 @@
+docker run \
+    -p 9090:9090 \
+    -v /home/zgao/Doc/TU/tu2k22-zhiyuangao/tu/prometheus.yml:/etc/prometheus/prometheus.yml \
+    prom/prometheus
